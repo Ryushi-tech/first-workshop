ccc3062b8c7e10b6531894f0f1a72f99c2f0422b
Ryushi-tech <52338292+Ryushi-tech@users.noreply.github.com>
2019-06-30 01:23:12 +0900

    Add files via upload

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sax&w()$$7m@r6q4-nu61v0@a0bruikpd#@8nhf1&@osa&1u)z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
